231d4ba811bc0af492060a28e04eab1e4faa0562
Aayush-2103 <2103aayush@gmail.com>
2024-03-28 08:46:45 +0530

    final code

--- Lottery.py
+++ Lottery.py
@@ -1,22 +1,23 @@
-while True:
-    print('\t\t*****Your own CALCULATOR*****\n')
-    a=float(input('1st number:- '))
-    b=float(input('2nd number:- '))
-    c=input('Operator:- ')
+#lottery number
+from random import randint
+ll=int(input('Enter the lower limit of your range:- '))
+ul=int(input('Enter the upper limit of your range:- '))
+n=randint(ll,ul)
+t=int(input('\n\nEnter number of times you wanna attempt:- '))
 
-    if c=='+':
-        print('Result ', a+b, '\n')
+while t!=0:
+    a=int(input('Enter any number between your provided range:- '))
+    if a==n:
+        print('Hurray! You did it champ\n')
+        break
 
-    elif c=='-':
-        print('Result ', a-b, '\n')
+    elif a>n and a<=ul:
+        print('You guessed it too high\n')
 
-    elif c=='*':
-        print('Result ', a*b, '\n')
-    
-    elif c=='/':
-        print('Result ', a/b, '\n')
+    elif a<n and a>=ll:
+        print('You guessed it too low\n')
 
     else:
-        print('Enter Valid operator\n\n')
-        continue
-    
+        print('Please enter within the range\n')
+
+    t-=1
